cc2e65965d4e6b22e9c2e78e3ac21110a3307043
Olivia Klemmer <oklemmer3@gatech.edu>
2022-09-13 17:28:06 -0400

    multiple inputs & num of electrode fixed

--- tests/test_lfp_amplitude_function.py
+++ tests/test_lfp_amplitude_function.py
@@ -1,58 +1,77 @@
+from cgi import test
 import numpy as np
-import lfp_amplitude_function 
-def test_ComputeLength():
+import lfp_amplitude_function
+
+def test_compute_length():
     # check that length is 0 when points are the same
-    length = lfp_amplitude_function.computeLength([10,10,10], [10,10,10])
-    assert length == 0
+    cords = np.random.rand(5, 3) * 10
+    length = lfp_amplitude_function.compute_length(cords, cords)
+    for arr in range(len(length)):
+        assert length[arr] == 0
     # check that flipping the neuron results in the same length
-    length1 = lfp_amplitude_function.computeLength([0,2,0], [0,0,0])
-    length2 = lfp_amplitude_function.computeLength([0,0,0], [0,2,0])
-    assert length1 == length2
+    length1 = lfp_amplitude_function.compute_length([[0,2,0]], [[0,0,0]])
+    length2 = lfp_amplitude_function.compute_length([[0,0,0]], [[0,2,0]])
+    for arr in range(len(length1)):
+        assert length1[arr] == length2[arr]
 
-def test_ComputeMidpoint():
+def test_compute_midpoint():
     # check that midpoint is the same if the neuron is flipped
-    midpoint1 = lfp_amplitude_function.computeMidPoint([0,2,0], [0,0,0])
-    midpoint2 = lfp_amplitude_function.computeMidPoint([0,0,0], [0,2,0])
-    assert midpoint1 == midpoint2
+    midpoint1 = lfp_amplitude_function.compute_midpoint([[0,2,0]], [[0,0,0]])
+    midpoint2 = lfp_amplitude_function.compute_midpoint([[0,0,0]], [[0,2,0]])
+    for arr in range(len(midpoint1)):
+        assert midpoint1[arr] == midpoint2[arr]
     # check that fucntion can handle negative values
-    midpoint = lfp_amplitude_function.computeMidPoint([0,-1,-3], [4,8,-10])
-    assert midpoint == [2, 3.5, -6.5]
+    midpoint = lfp_amplitude_function.compute_midpoint([[0,-1,-3]], [[4,8,-10]])
+    for arr in range(len(midpoint)):
+        assert midpoint[arr] == [2, 3.5, -6.5]
 
-def test_ComputeDistance():
+def test_compute_distance():
     # check that distance is negative when electrode is below neuron
-    distance = lfp_amplitude_function.computeDistance([0,2,0], [0,0,0])
-    assert distance <= 0
+    distance = lfp_amplitude_function.compute_distance([[0,2,0]], [[0,0,0]])
+    for arr in range(len(distance)):
+        assert distance[arr] <= 0
     # check that distance is positive when electrode is above neuron
-    distance1 = lfp_amplitude_function.computeDistance([0,2,0], [0,4,0])
-    assert distance1 >= 0
+    distance1 = lfp_amplitude_function.compute_distance([[0,2,0]], [[0,4,0]])
+    for arr in range(len(distance1)):
+        assert distance1[arr] >= 0
     # check that distance is smaller when electrode is closer to neuron and vise versa
-    distance2 = lfp_amplitude_function.computeDistance([0,0,0], [0,4,0])
-    assert distance1 < distance2
+    distance2 = lfp_amplitude_function.compute_distance([[0,0,0]], [[0,4,0]])
+    for arr in range(len(distance2)):
+        assert distance1[arr] < distance2[arr]
     # check that distance is inverse when electrode is rotated around neuron
-    distance3 = lfp_amplitude_function.computeDistance([0,0,0], [0,-4,0])
-    assert -distance2 == distance3
+    distance3 = lfp_amplitude_function.compute_distance([[0,0,0]], [[0,-4,0]])
+    for arr in range(len(distance3)):
+        assert -distance2[arr] == distance3[arr]
 
-def test_ComputeAngle():
+def test_compute_angle():
     # check that angle is the same if the electrode is above or below the neuron
-    angle1 = lfp_amplitude_function.computeAngle([0,2,0], [0,1,0])
-    angle2 = lfp_amplitude_function.computeAngle([0,2,0], [0,4,0])
+    angle1 = lfp_amplitude_function.compute_angle([[0,2,0]], [[0,1,0]])
+    angle2 = lfp_amplitude_function.compute_angle([[0,2,0]], [[0,4,0]])
     assert angle1 == angle2
 
-def test_ComputeAmp():
+def test_compute_amp():
     # check that LFP Amplitude is positive above the midpoint
-    amp1 = lfp_amplitude_function.computeAmp([[1,1,1]], [[2,2,2]], [[5,5,5]])
-    assert amp1 >= 0
+    amp1 = lfp_amplitude_function.compute_amp([[1,1,1]], [[2,2,2]], [[5,5,5]])
+    for arr in range(len(amp1)):
+        assert amp1[arr] >= 0
     # check that LFP Amplitude is negarive below the midpoint
-    amp2 = lfp_amplitude_function.computeAmp([[1,1,1]], [[2,2,2]], [[-5,-5,-5]])
-    assert amp2 <= 0
+    amp2 = lfp_amplitude_function.compute_amp([[1,1,1]], [[2,2,2]], [[-5,-5,-5]])
+    for arr in range(len(amp2)):
+        assert amp2[arr] <= 0
     # check that LFP Amplitude is stronger closer to the neuron
-    amp3 = lfp_amplitude_function.computeAmp([[1,1,1]],[[2,2,2]],[[3,3,3]])
-    amp4 = lfp_amplitude_function.computeAmp([[1,1,1]],[[2,2,2]],[[6,6,6]])
-    assert amp3 > amp4
+    amp3 = lfp_amplitude_function.compute_amp([[1,1,1]],[[2,2,2]],[[3,3,3]])
+    amp4 = lfp_amplitude_function.compute_amp([[1,1,1]],[[2,2,2]],[[6,6,6]])
+    for arr in range(len(amp3)):
+        assert amp3[arr] > amp4[arr]
     # check that flipping the neuron results in the same LFP
-    amp5 = lfp_amplitude_function.computeAmp([[1,1,1]],[[2,2,2]],[[3,3,3]])
-    amp6 = lfp_amplitude_function.computeAmp([[2,2,2]],[[1,1,1]],[[3,3,3]])
-    assert amp5 == amp6
+    amp5 = lfp_amplitude_function.compute_amp([[1,1,1]],[[2,2,2]],[[3,3,3]])
+    amp6 = lfp_amplitude_function.compute_amp([[2,2,2]],[[1,1,1]],[[3,3,3]])
+    for arr in range(len(amp5)):
+        assert amp5[arr] == amp6[arr]
+    # check input of multiple neurons
+    soma = np.random.rand(5, 3) * 10
+    dendrite = np.random.rand(5, 3) * 10
+    electrode = np.random.rand(5, 3) * 10
 
 def test_random_orientation():
     rot_mat = rotation_matrix()
@@ -62,9 +81,10 @@ def test_random_orientation():
     soma2 = np.dot(rot_mat, soma1)
     dendrite2 = np.dot(rot_mat, dendrite1)
     electrode2 = np.dot(rot_mat, electrode1)
-    amp1 = lfp_amplitude_function.computeAmp([soma1], [dendrite1], [electrode1])
-    amp2 = lfp_amplitude_function.computeAmp([soma2], [dendrite2], [electrode2])
-    assert amp1 == amp2
+    amp1 = lfp_amplitude_function.compute_amp([soma1], [dendrite1], [electrode1])
+    amp2 = lfp_amplitude_function.compute_amp([soma2], [dendrite2], [electrode2])
+    for arr in range(len(amp1)):
+        assert amp1[arr] == (amp2[arr])
     
 def rotation_matrix(rng: np.random.Generator = np.random.default_rng()):
     τ = 2 * np.pi
@@ -76,3 +96,25 @@ def rotation_matrix(rng: np.random.Generator = np.random.default_rng()):
         [[1, 0, 0], [0, np.cos(θyz), -np.sin(θyz)], [0, np.sin(θyz), np.cos(θyz)]]
     )
     return rot_mat
+
+def test_multiple_inputs():
+    soma = np.random.rand(5, 3) * 10
+    dendrite = np.random.rand(5, 3) * 10
+    electrode = np.random.rand(5, 3) * 10
+    midpoint = lfp_amplitude_function.compute_midpoint(soma, dendrite)
+    print(lfp_amplitude_function.compute_amp(soma, dendrite, electrode))
+
+def test_fewer_electrodes():
+    soma = np.random.rand(5, 3) * 10
+    dendrite = np.random.rand(5, 3) * 10
+    electrode = np.random.rand(2, 3) * 10
+    print(lfp_amplitude_function.compute_amp(soma, dendrite, electrode))
+
+test_compute_amp()
+test_compute_angle()
+test_compute_distance()
+test_compute_length()
+test_compute_midpoint()
+test_multiple_inputs()
+test_random_orientation()
+test_fewer_electrodes()
--- tklfp/lfp_amplitude_function.py
+++ tklfp/lfp_amplitude_function.py
@@ -4,61 +4,81 @@ from random import randrange
 import numpy as np
 import math
 import random
+
 """
-soma: array containing the x, y, and z position coordinates of the soma
-dendrite: array containing the x, y, and z position coordinates of the dendrites
-electrode: array containing the x, y, and z position coordinates of the electrode
+* imput params: matrix of arrays representing individual neurons
+* each array houses x, y, and z coordinates of respective structures
+* ex: soma = [[1,1,1],
+              [2,2,2],
+              [3,3,3]]
+* must be same number of soma and dendrite coordinates, i.e. each neuron has a soma and dendrite point in space
+* each neuron maps to each electrode
 """
 
-def computeAmp (soma, dendrite, electrode):
-    lfp_amp_arr = np.zeros(len(soma))
+def compute_amp (soma, dendrite, electrode):
+    length = compute_length(soma, dendrite)
+    midpoint = compute_midpoint(soma, dendrite)
+    distance = compute_distance(midpoint, electrode)
+    angle = compute_angle(midpoint, electrode)
+    LFP_amp_arr = []
+    for arr in range(len(electrode)):
+        for num in range(len(soma)):
+            LFP_amp = (length[arr] * math.cos(angle[num])) / (4 * math.pi * 0.3 * distance[num] ** 2)
+            LFP_amp_arr.append(round(LFP_amp, 15))
+    return LFP_amp_arr
+
+def compute_length (soma, dendrite):
+    length_arr = []
     for arr in range(len(soma)):
-        length = computeLength(soma[arr], dendrite[arr])
-        midpoint = computeMidPoint(soma[arr], dendrite[arr])
-        distance = computeDistance(midpoint, electrode[arr])
-        angle = computeAngle(midpoint, electrode[arr])
-        LFPAmp = (length * math.cos(angle)) / (4 * math.pi * 0.3 * distance ** 2)
-        lfp_amp_arr[arr] = LFPAmp
-    return lfp_amp_arr
+        x1 = soma[arr][0]
+        x2 = dendrite[arr][0]
+        y1 = soma[arr][1]
+        y2 = dendrite[arr][1]
+        z1 = soma[arr][2]
+        z2 = dendrite[arr][2]
+        
+        length = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+        length_arr.append(length)
+    return length_arr
 
-def computeLength (soma, dendrite):
-    x1 = soma[0]
-    x2 = dendrite[0]
-    y1 = soma[1]
-    y2 = dendrite[1]
-    z1 = soma[2]
-    z2 = dendrite[2]
-    length = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
-    return length
+def compute_midpoint (soma, dendrite):
+    midpoint_arr = []
+    for arr in range(len(soma)):
+        x1 = soma[arr][0]
+        x2 = dendrite[arr][0]
+        y1 = soma[arr][1]
+        y2 = dendrite[arr][1]
+        z1 = soma[arr][2]
+        z2 = dendrite[arr][2]
+        midpoint = [(x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2]
+        midpoint_arr.append(midpoint)
+    return midpoint_arr
 
-def computeMidPoint (soma, dendrite):
-    x1 = soma[0]
-    x2 = dendrite[0]
-    y1 = soma[1]
-    y2 = dendrite[1]
-    z1 = soma[2]
-    z2 = dendrite[2]
-    midpoint = [(x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2]
-    print(midpoint)
-    return midpoint
+def compute_distance (midpoint, electrode):
+    distance_arr = []
+    for arr in range(len(electrode)):
+        for num in range(len(midpoint)):
+            x1 = midpoint[num][0]
+            x2 = electrode[arr][0]
+            y1 = midpoint[num][1]
+            y2 = electrode[arr][1]
+            z1 = midpoint[num][2]
+            z2 = electrode[arr][2]
+            distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+            if (y1 > y2):
+                distance = -distance
+            distance_arr.append(distance)
+    return distance_arr
 
-def computeDistance (midpoint, electrode):
-    x1 = midpoint[0]
-    x2 = electrode[0]
-    y1 = midpoint[1]
-    y2 = electrode[1]
-    z1 = midpoint[2]
-    z2 = electrode[2]
-    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
-    if (y1 > y2):
-        return -distance
-    else:
-        return distance
-
-def computeAngle (midpoint, electrode):
-    unitVector1 = midpoint / np.linalg.norm(midpoint)
-    unitVector2 = electrode / np.linalg.norm(electrode)
-    dotProduct = np.dot(unitVector1, unitVector2)
-    angle = np.arccos(dotProduct)
-    return angle
-
+def compute_angle (midpoint, electrode):
+    angle_arr = []
+    for arr in range(len(electrode)):
+        for num in range(len(midpoint)):
+            unit_vector1 = midpoint[num] / np.linalg.norm(midpoint[num])
+            unit_vector2 = electrode[arr] / np.linalg.norm(electrode[arr])
+            dot_product = np.dot(unit_vector1, unit_vector2)
+            if dot_product > 1:
+                dot_product = 1
+            angle = np.arccos(dot_product)
+            angle_arr.append(angle)
+    return angle_arr
