b7129e97ba972583924c28c51e55223a434c4ced
AlissaW0921 <awcy0921@gmail.com>
2023-12-01 03:38:07 -0500

    Update test_wslfp.py

--- tests/test_wslfp.py
+++ tests/test_wslfp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from src import WSLFP
+import matplotlib.pyplot as plt
 
 @pytest.mark.parametrize(
     "ampa_times, gaba_times, t_evals, success, lfp_increasing",
@@ -12,12 +13,14 @@ from src import WSLFP
         ([], [], [], False, False),  # Empty arrays, should fail
         ([1, 5, 9, 12, 17], [1, 3, 5, 9, 12], [8, 11], True, True),
         ([1, 5, 9, 12, 17], [1, 3, 5, 9, 12], [], False, False),
-        ([2, 4, 6, 23, 25], [7, 10, 15, 21, 25], [12, 20, 24], True, True)
+        ([2, 4, 6, 23, 25], [7, 10, 15, 21, 25], [12, 20, 24], True, True),
+        ([2, 3, 4, 5, 6], [50, 60, 70, 80, 90], [70, 100], False, False)
     ]
 )
 def test_wslfp_time_ranges(ampa_times, gaba_times, t_evals, success, lfp_increasing):
     # Dummy data for currents and coordinates
-    ampa_currents = gaba_currents = np.array([1, 2, 3, 4, 5])
+    ampa_currents = np.array([1, 2, 3, 4, 5])
+    gaba_currents = np.array([-1, -2, -3, -4, -5])
     xs = ys = zs = np.array([1, 2, 3])
     elec_coords = np.array([[1, 1, 1], [2, 2, 2]])
 
