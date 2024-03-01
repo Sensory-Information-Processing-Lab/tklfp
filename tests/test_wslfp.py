5a6b9d24a6c985c2f1ee5f537f5ca75d20ba672d
Kyle Johnsen <johnsenkyle13@gmail.com>
2024-02-29 22:24:29 -0500

    progress on spike computation

--- tests/test_wslfp.py
+++ tests/test_wslfp.py
@@ -1,39 +1,53 @@
 import numpy as np
 import pytest
+
 from wslfp import WSLFP
-import matplotlib.pyplot as plt
+
 
 @pytest.mark.parametrize(
-    "ampa_times, gaba_times, t_evals, success, lfp_increasing",
+    "t_ampa, t_gaba, t_eval, success, lfp_increasing",
     [
-        ([0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], False, False),  # Valid times, should pass
-        ([5, 6, 7, 8, 9], [5, 6, 7, 8, 9], [10, 11, 12], False, False),  # t_evals out of range, should fail
-        ([1, 2, 4], [1, 2, 10], [9], False, False),  # length of time and current array must equal
+        # Valid times, should pass
+        ([0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], False, False),
+        # t_evals out of range, should fail
+        ([5, 6, 7, 8, 9], [5, 6, 7, 8, 9], [10, 11, 12], False, False),
+        # length of time and current array must equal
+        ([1, 2, 4], [1, 2, 10], [9], True, True),
+        # missing I_gaba at t=9
+        ([1, 2, 10], [1, 2, 4], [9], False, False),
         ([3, 2, 1], [3, 2, 1], [2], False, False),  # Non-increasing order, should fail
         ([], [], [], False, False),  # Empty arrays, should fail
         ([1, 5, 9, 12, 17], [1, 3, 5, 9, 12], [8, 11], True, True),
         ([1, 5, 9, 12, 17], [1, 3, 5, 9, 12], [], False, False),
         ([2, 4, 6, 23, 25], [7, 10, 15, 21, 25], [12, 20, 24], True, True),
-        ([2, 3, 4, 5, 6], [50, 60, 70, 80, 90], [70, 100], False, False)
-    ]
+        ([2, 3, 4, 5, 6], [50, 60, 70, 80, 90], [70, 100], False, False),
+    ],
 )
-def test_wslfp_time_ranges(ampa_times, gaba_times, t_evals, success, lfp_increasing):
+@pytest.mark.parametrize("n_elec", [1, 20])
+def test_time_ranges(t_ampa, t_gaba, t_eval, success, lfp_increasing, n_elec):
     # Dummy data for currents and coordinates
-    ampa_currents = np.array([1, 2, 3, 4, 5])
-    gaba_currents = np.array([-1, -2, -3, -4, -5])
-    xs = ys = zs = np.array([1, 2, 3])
-    elec_coords = np.array([[1, 1, 1], [2, 2, 2]])
+    n_src = 3
+    n_elec = 2
+    I_ampa = np.arange(len(t_ampa))
+    I_ampa = I_ampa[:, np.newaxis] + np.arange(n_src)
+    I_gaba = -np.arange(len(t_gaba)) - 2
+    I_gaba = I_gaba[:, np.newaxis] - np.arange(n_src)
 
-    wslfp = WSLFP(xs, ys, zs, elec_coords, ampa_times, ampa_currents, gaba_times, gaba_currents)
+    xs = ys = zs = np.array([1, 2, 3])
+    source_coords = np.column_stack((xs, ys, zs))
+    elec_coords = np.random.uniform(-500, 500, (n_elec, 3))
+
+    wslfp = WSLFP.from_xyz_coords(elec_coords, source_coords, strict_boundaries=True)
     if success:
-        try:
-            lfp_values = wslfp.calculate_lfp(t_evals)
-            print("Calculated LFP values:", lfp_values)
-            assert lfp_increasing == np.all(np.diff(lfp_values) >= 0), "LFP increasing order does not match expectation"
-        except ValueError:
-            assert False, "Test expected to succeed but failed"
+        lfp_values = wslfp.compute(t_ampa, I_ampa, t_gaba, I_gaba, t_eval)
+        assert lfp_values.shape == (len(t_eval), n_elec)
+        assert lfp_increasing == np.all(
+            np.diff(lfp_values, axis=0) >= 0
+        ), "LFP increasing order does not match expectation"
     else:
         with pytest.raises(ValueError):
-            wslfp.calculate_lfp(t_evals)
+            wslfp.compute(t_ampa, I_ampa, t_gaba, I_gaba, t_eval)
 
 
+if __name__ == "__main__":
+    pytest.main([__file__, "--lf", "-x", "-s"])
--- wslfp/__init__.py
+++ wslfp/__init__.py
@@ -1,7 +1,9 @@
 import warnings
 
 import numpy as np
+import numba
 from attrs import define, field
+from scipy import sparse
 from scipy.interpolate import PchipInterpolator
 
 from wslfp import amplitude
@@ -115,7 +117,7 @@ class WSLFP:
         if not np.all(np.diff(t_ms) > 0):
             raise ValueError("t_ms must be monotonically increasing")
 
-        t_eval_delayed = t_eval_ms - delay_ms
+        t_eval_delayed = np.subtract(t_eval_ms, delay_ms)
         t_needed = (np.min(t_eval_delayed), np.max(t_eval_delayed))
         t_provided = (np.min(t_ms), np.max(t_ms))
 
@@ -135,10 +137,11 @@ class WSLFP:
                 )
         # I_interp = np.interp(t_eval_delayed, t_ms, I, left=0, right=0)
         I_interp = PchipInterpolator(t_ms, I, extrapolate=False)(t_eval_delayed)
+        assert I_interp.shape == (len(t_eval_ms), self.n_sources)
         I_interp[np.isnan(I_interp)] = 0
         return I_interp
 
-    def compute(self, t_ampa_ms, I_ampa, t_gaba_ms, I_gaba, t_eval_ms, normalize=True):
+    def compute(self, t_eval_ms, t_ampa_ms, I_ampa, t_gaba_ms, I_gaba, normalize=True):
         I_ampa = np.reshape(I_ampa, (-1, self.n_sources))
         assert I_ampa.shape == (
             len(t_ampa_ms),
@@ -158,106 +161,119 @@ class WSLFP:
         )
 
         wsum = self.amp_uV * (I_ampa_eval - self.alpha * I_gaba_eval)[:, np.newaxis, :]
+        assert wsum.shape == (len(t_eval_ms), self.n_elec, self.n_sources)
         wsum = np.sum(wsum, axis=2)
         assert wsum.shape == (len(t_eval_ms), self.n_elec)
         if normalize:
-            wsum = (wsum - np.mean(wsum, axis=0)) / np.std(wsum, axis=0)
+            wsum = wsum - np.mean(wsum, axis=0)
+            if len(t_eval_ms) > 1:
+                wsum /= np.std(wsum, axis=0)
             assert wsum.shape == (len(t_eval_ms), self.n_elec)
 
         assert np.allclose(wsum.mean(axis=0), 0)
-        assert np.allclose(wsum.std(axis=0), 1)
+        if len(t_eval_ms) > 1:
+            assert np.allclose(wsum.std(axis=0), 1)
 
-        wsum *= self.amp_uV.mean(axis=1)
+        wsum *= np.abs(self.amp_uV.mean(axis=1))
         return wsum
 
 
-class WSLFP2:
-    def __init__(
-        self,
-        xs,
-        ys,
-        zs,
-        elec_coords,
-        ampa_times,
-        ampa_currents,
-        gaba_times,
-        gaba_currents,
-        alpha=1.65,
-        tau_ampa_ms=6,
-        tau_gaba_ms=0,
-    ):
-        """xs, ys, zs are n_neurons-length 1D arrays
-        elec_coords is a (n_elec, 3) array
-        [[1, 1, 1],
-         [2, 2, 2]]"""
-        self.xs = xs
-        self.ys = ys
-        self.zs = zs
-        self.elec_coords = elec_coords
-        self.ampa_times = ampa_times
-        self.ampa_currents = ampa_currents
-        self.gaba_times = gaba_times
-        self.gaba_currents = gaba_currents
-        self.alpha = alpha
-        self.tau_ampa_ms = tau_ampa_ms
-        self.tau_gaba_ms = tau_gaba_ms
-
-    def get_current(self, times, currents, t_eval, kind="linear"):
-        if len(times) != len(currents):
-            raise ValueError("Length of times and currents arrays must be equal.")
-        if len(times) == 1:
-            if np.all(times[0] == t_eval):
-                return np.full_like(t_eval, currents[0])
-            else:
-                raise ValueError(
-                    "Evaluation time does not match the single available time point."
-                )
-        else:
-            if np.any(t_eval < times[0]) or np.any(t_eval > times[-1]):
-                raise ValueError("Evaluation time is out of the current data range.")
-            interpolate = interp1d(times, currents, kind=kind, bounds_error=True)
-            return interpolate(t_eval)
-
-    def normalize(self, data):
-        return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-    def calculate_lfp(self, t_evals):
-        t_evals = np.array(t_evals)
-        if len(t_evals) == 0:
-            raise ValueError("Evaluation time is not given.")
-        if len(self.ampa_times) == 0 or len(self.gaba_times) == 0:
-            raise ValueError("AMPA times and GABA times cannot be empty")
-        if not np.all(np.diff(self.ampa_times) >= 0):
-            raise ValueError("AMPA times are not in increasing order")
-        if not np.all(np.diff(self.gaba_times) >= 0):
-            raise ValueError("GABA times are not in increasing order")
-        if np.any(t_evals - self.tau_ampa_ms < self.ampa_times[0]) or np.any(
-            t_evals - self.tau_ampa_ms > self.ampa_times[-1]
-        ):
-            raise ValueError("Some evaluation times are out of the AMPA time range.")
-        if np.any(t_evals - self.tau_gaba_ms < self.gaba_times[0]) or np.any(
-            t_evals - self.tau_gaba_ms > self.gaba_times[-1]
-        ):
-            raise ValueError("Some evaluation times are out of the GABA time range.")
-        ampa_currents = np.array(
-            [
-                self.get_current(
-                    self.ampa_times, self.ampa_currents, t_eval - self.tau_ampa_ms
-                )
-                for t_eval in t_evals
-            ]
+@numba.njit
+def biexp_kernel(t, tau1, tau2, normalize=False):
+    """Biexponential kernel"""
+    if normalize:
+        tau_rise = tau1 * tau2 / (tau1 - tau2)
+        B = 1 / (
+            (tau2 / tau1) ** (tau_rise / tau1) - (tau2 / tau1) ** (tau_rise / tau2)
         )
-        gaba_currents = np.array(
-            [
-                self.get_current(
-                    self.gaba_times, self.gaba_currents, t_eval - self.tau_gaba_ms
-                )
-                for t_eval in t_evals
-            ]
-        )
-        ws = ampa_currents - self.alpha * gaba_currents
-        amplitudes = lfp_amplitude_function.compute_amp(
-            [[1, 1, 1]], [[2, 2, 2]], [[5, 5, 5]]
-        )
-        lfp_normalized = self.normalize(amplitudes * ws)
-        return lfp_normalized
+    else:
+        B = 1
+    print(np.sum(np.isnan((np.exp(-t / tau1) - np.exp(-t / tau2)) * (t >= 0))))
+    # print(np.shape(np.exp(-t / tau1) - np.exp(-t / tau2)))
+    # print(np.shape(t >= 0))
+    # print(np.sum(np.isnan(t >= 0)))
+    return B * (np.exp(-t / tau1) - np.exp(-t / tau2)) * (t >= 0)
+
+
+@numba.njit
+def _postsyn_spikes_to_biexp_currents(
+    t_eval_ms, t_spk_ms, j_spk, J_spk, n_curr_sources, tau1, tau2, normalize
+):
+    n_spk = len(t_spk_ms)
+    T = len(t_eval_ms)
+
+    # # uses a crazy amount of memory since there are so many postsynaptic spikes
+    # t_spk_conv = t_eval_ms[..., np.newaxis] - t_spk_ms
+    # assert t_spk_conv.shape == (T, n_spk)
+
+    # I_syn_per_spike = kernel(t_spk_conv, **kernel_kwargs) * J_spk
+    # assert I_syn_per_spike.shape == (T, n_spk)
+
+    # # need to sum across spikes that go to same current source
+    # I_syn_per_src = np.zeros((T, n_curr_sources))
+    # for j_src in range(n_curr_sources):
+    #     I_syn_per_src[:, j_src] = np.sum(I_syn_per_spike[:, j_spk == j_src], axis=1)
+    I_syn_per_src = np.zeros((T, n_curr_sources))
+    for i_t, t in enumerate(t_eval_ms):
+        t_spk_conv = t - t_spk_ms
+        assert t_spk_conv.shape == (n_spk,)
+        I_syn_per_spike = biexp_kernel(t_spk_conv, tau1, tau2, normalize) * J_spk
+        # print(np.isnan(I_syn_per_spike).sum())
+        for j_src, I_syn in zip(j_spk, I_syn_per_spike):
+            I_syn_per_src[i_t, j_src] += I_syn
+
+    return I_syn_per_src
+
+
+# def spikes_to_currents(
+#     t_eval_ms, t_spk_ms, i_spk, J, kernel, syn_delay_ms, **kernel_kwargs
+# ):
+def spikes_to_biexp_currents(
+    t_eval_ms, t_spk_ms, i_spk, J, tau1, tau2, syn_delay_ms=1, normalize=False
+):
+    n_each_spk = (J[i_spk, :] != 0).sum(axis=1)
+    t_postsyn_spk_ms = np.repeat(t_spk_ms + syn_delay_ms, n_each_spk)
+    i_postsyn_spk = np.repeat(i_spk, n_each_spk)
+
+    if isinstance(J, np.ndarray):
+        i_postsyn_spk = np.repeat(i_spk, n_each_spk)
+        assert len(t_postsyn_spk_ms) == np.sum(n_each_spk)
+
+        spike_index, j_postsyn_spk = np.nonzero(J[i_spk, :])
+        J_postsyn_spk = J[i_postsyn_spk, j_postsyn_spk]
+
+    elif sparse.issparse(J):
+        spike_index, j_postsyn_spk, J_postsyn_spk = sparse.find(J[i_spk, :])
+
+    assert np.all(np.diff(spike_index) >= 0), "assuming nonzero goes by row"
+    assert i_postsyn_spk.shape == j_postsyn_spk.shape == J_postsyn_spk.shape
+
+    return _postsyn_spikes_to_biexp_currents(
+        t_eval_ms,
+        t_postsyn_spk_ms,
+        j_postsyn_spk,
+        J_postsyn_spk,
+        J.shape[1],
+        tau1,
+        tau2,
+        normalize,
+    )
+
+
+# def spikes_to_biexp_currents(
+#     t_eval_ms, t_spk_ms, i_spk, J, tau1, tau2, syn_delay_ms=1, normalize=False
+# ):
+#     """J[i, j] = weight from source i to target j
+#     tau1: fall time constant. 2 ms for AMPA, 5 ms for GABA
+#     tau2: rise time constant. 0.4 ms for AMPA, 0.25 ms for GABA"""
+#     return spikes_to_currents(
+#         t_eval_ms,
+#         t_spk_ms,
+#         i_spk,
+#         J,
+#         biexp_kernel,
+#         syn_delay_ms,
+#         tau1=tau1,
+#         tau2=tau2,
+#         normalize=normalize,
+#     )
